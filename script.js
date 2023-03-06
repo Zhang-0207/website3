67bd46aa0afe35d562b1fd79ff71fd528a5396c0
Zhang-0207 <a0909964563@gmail.com>
2023-03-06 20:49:34 +0800

    OK

---
+++ script.js
@@ -0,0 +1,12 @@
+let title = "密碼輸入:"   
+let title1 = "正確"   
+let title2 = "錯誤"   
+let hint = title + title1
+let hint1 = title + title2
+let webSite_Age = 0
+let isTrue = true
+
+console.log(hint)
+console.log(hint1)
+
+
---
+++ script2.js
@@ -0,0 +1,5 @@
+let classA = ["A","B","C","D","E","F","G","H","I","J",]
+console.log(classA);
+classA.push('K')
+console.log("增加後"+classA);
+console.log("人數:"+classA.length+"人")
